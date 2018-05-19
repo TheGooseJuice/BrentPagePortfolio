72560c133ca2c269edc80b94bf405f71d9b78f6f
Brent Page <Brentpage75@gmail.com>
2018-05-19 18:53:10 -0400

    making game space smaller

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 var config = {
     type: Phaser.AUTO,
-    width:   window.innerWidth,
-    height: window.innerHeight,
+    width:   window.innerWidth/2,
+    height: window.innerHeight/2,
     physics: {
         default: 'arcade',
         arcade: {
